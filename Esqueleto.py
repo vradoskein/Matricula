01330f2a82698564163cb6b34452958a8233f00b
vradoskein <joaorener@gmail.com>
2019-12-10 13:30:20 -0800

    Creating the buttons to mount the timetable, logic not implemented yet

--- Esqueleto.py
+++ Esqueleto.py
@@ -2,8 +2,6 @@ from tkinter import *
 from Materia import Materia
 
 #Arquivo para ser importado
-
-
 filename = "test.txt"
 
 #Inicializa lita com todas materias
@@ -15,20 +13,60 @@ tentativa.geometry("900x900")
 tentativa.title("Auxilio em matricula")
 var = dict()
 
+#variaveis criadas para auxiliar
 linhacheck = 1
 columncheck =1
+
+#criar grade com o maior numero de materias
+def Maxmat(event):
+    pass
+
+#criar grade com o numero N de materias
+def onButtonClick():
+    pass
+
+
+#Cria opcoes para a pessoa escolher como montar a grade
+#N SEI PQ Q N TA DANDO PRA PEGAR
+#A VARIAVEL GLOBAL FOI O UNICO JEITO Q EU CONSGUI.....
+def createoptions():
+    grades = Label(tentativa, text='Selecione estilo:')
+    grades.grid(row=17, column=1)
+
+    max = Button(tentativa, text="Grade com mais materias", background = 'orange')
+    max.bind("<Button-1>", Maxmat)
+    max.grid(row=18, column=1)
+
+    max = Button(tentativa, text="Grade com N materias", background='orange', command=onButtonClick)
+    max.grid(row=18, column=2)
+
+    global vai
+    vai = IntVar()
+    en = Entry(tentativa, textvariable=vai)
+    en.grid(row=18, column=3)
+
 
 #Verifica quais materias estao able e mostra elas na tela
 def ablemat(linha, coluna):
     for x in allmat:
+        x.isable()
         if x.able and not x.done:
             var[x]=IntVar()
-            check2 = Button(tentativa, text = x.name, command = var[x])
-            check2.grid(row=linha, column = coluna, sticky=W)
+            if x.peso > 3 :
+                check2 = Button(tentativa, text=x.name, background = 'red' ,command=var[x])
+                check2.grid(row=linha, column=coluna, sticky=W)
+            elif x.peso > 0:
+                check2 = Button(tentativa, text = x.name, background = 'yellow', command = var[x])
+                check2.grid(row=linha, column = coluna, sticky=W)
+            else:
+                check2 = Button(tentativa, text=x.name, command=var[x])
+                check2.grid(row=linha, column=coluna, sticky=W)
             linha += 1
             if linha == 16:
-                coluna += 2
+                coluna += 1
                 linha = 10
+
+    createoptions()
 
 #seta pra done as materias ja feitas
 def Lecheck(event):
@@ -39,7 +77,6 @@ def Lecheck(event):
 
 #Montando grid de check boxes
 for x in allmat:
-    print(x.name,x.reqs, x.peso)
     var[x]=IntVar()
     check1 = Checkbutton(tentativa, text = x.name,  variable=var[x])
     check1.grid(row=linhacheck, column=columncheck, sticky=W)
@@ -53,7 +90,7 @@ for x in allmat:
 button_concluir = Button(tentativa, text="Concluir materias")
 button_concluir.bind("<Button-1>", Lecheck)
 button_concluir.grid(row=8, column=1)
-label = Label(tentativa, text = 'MATERIAS DISPONIVEIS')
+label = Label(tentativa, text = 'MATERIAS DISPONIVEIS:')
 label.grid(row = 9, column= 1)
 
 
--- Materia.py
+++ Materia.py
@@ -2,13 +2,13 @@ class Materia:
     matlist =[]
 
     #Estrutura de materias e construtor
-    def __init__(self, name, reqs):
+    def __init__(self, name, reqs, horario):
         self.name = name
         self.reqs = reqs
         self.done = False
         self.able = False
         self.peso = 0
-#       self.horario = horario
+        self.horario = horario
 
     #settar estado da materia para done
     def setDone(self):
@@ -37,13 +37,11 @@ class Materia:
         else:
             self.able = True
 
+    #define o peso de cada materia, com base nas outras em que ela eh pre requisito
     def pesos(self):
         for m in Materia.matlist:
             if self.name in m.reqs:
                 self.peso += 1
-
-
-
 
     #Metodo para criar a lista de materias
     def allmat(filename):
@@ -53,10 +51,10 @@ class Materia:
         for linha in file:
             mat = linha.split(":")
             requisitos = mat[1].rstrip("\n").split(",")
-            Materia.matlist.append(Materia(mat[0], requisitos))
+            horarios = mat[2].rstrip("\n").split(",")
+            Materia.matlist.append(Materia(mat[0], requisitos, horarios))
 
         for mat in Materia.matlist:
             mat.pesos()
-            mat.isable()
 
         return Materia.matlist
